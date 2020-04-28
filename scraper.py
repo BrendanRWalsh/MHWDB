c41955dd0c3d0d4b6bc1efb748c2cbf5b551c82e
Brendan Walsh <Brendanwalsh@live.com.au>
2020-04-28 12:17:54 +1000

    Basic web scraping setup

--- scraper.py
+++ scraper.py
@@ -1,1 +1,16 @@
-print("hello world!")
+import requests
+from bs4 import BeautifulSoup
+
+
+page = requests.get("https://mhworld.kiranico.com/weapons/r8H9i4/buster-sword-i")
+soup = BeautifulSoup(page.content, 'html.parser')
+# f = open("content.txt", "a")
+# for listitem in list(soup.children):
+#     f.write('%s\n' % listitem)
+# f.close()
+
+#item image is img-fluid
+name = soup.find_all(class_='align-self-center')[0].text
+thumb = soup.find_all(class_='img-fluid')[0]['src']
+print(name)
+print(thumb)
