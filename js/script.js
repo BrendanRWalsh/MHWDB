83b410aac96514b8060d949251f522c0fb9582ce
Brendan Walsh <Brendanwalsh@live.com.au>
2020-04-30 19:14:53 +1000

    Introduced search functions

--- js/script.js
+++ js/script.js
@@ -46,3 +46,27 @@ function calcItems(weapon) {
     }
     catch{ }
 }
+function findByName(weaponName){
+    lastChar = weaponName[weaponName.length-1].toLowerCase()
+    if (lastChar == 'i' || lastChar == 'v' || lastChar == 'x'){
+        space = weaponName.lastIndexOf(" ")
+        roman = ['i','ii','iii','iv','v','vi','vii','viii','ix']
+        weaponValue = weaponName.toLowerCase().substring(space + 1,weaponName.length)
+        if(roman.includes(weaponValue)){
+            x = roman.indexOf(weaponValue) + 1
+            weaponName = weaponName.substring(0,weaponName.length - weaponValue.length).concat(x.toString())
+            console.log(weaponName)
+        }
+    }
+    for(weapon in weapons){if (weapons[weapon].name == weaponName){console.log(weapons[weapon])}}
+}
+function findByType(weaponType){
+    list = []
+
+    for(weapon in weapons){
+        if (weapons[weapon].type == weaponType){
+            list.push(weapons[weapon])
+        }
+    }
+    return list
+}
