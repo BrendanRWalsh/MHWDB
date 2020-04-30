65c145c63535c42205d181353c027739ae5a6598
Brendan Walsh <Brendanwalsh@live.com.au>
2020-04-30 12:06:08 +1000

    initialized objects + added more elements

--- scraper.py
+++ scraper.py
@@ -1,13 +1,99 @@
 import requests
 from bs4 import BeautifulSoup
 # https://mhworld.kiranico.com/weapons/kkFocOG/magda-ungulae-ii
-
-page = requests.get("https://mhworld.kiranico.com/weapons/O5uZiXz/iron-eschaton-i")
+# https://mhworld.kiranico.com/weapons/R5HQFyG/ogres-jaw-iii
+page = requests.get("https://mhworld.kiranico.com/weapons/kkFocOG/magda-ungulae-ii")
 soup = BeautifulSoup(page.content, 'html.parser')
 # f = open("content.txt", "a")
 # f.write(soup.prettify())
 # f.close()
-obj = {}
+obj = {
+    "id" : 0,
+    "type" : "",
+    "rarity" : 0,
+    "attack" : {
+        "display": 0,
+        "raw": 0
+        },
+    "affinity" : 0,
+    "defense" : 0,
+    "elderseal" : "",
+    "attributes" : {},
+    "damageType" : "",
+    "name" : "",
+    "description" : "",
+    "durability" : [
+            {
+                "red": 0,
+                "orange": 0,
+                "yellow": 0,
+                "green": 0,
+                "blue": 0,
+                "white": 0,
+                "purple": 0
+            },
+            {
+                "red": 0,
+                "orange": 0,
+                "yellow": 0,
+                "green": 0,
+                "blue": 0,
+                "white": 0,
+                "purple": 0
+            },
+            {
+                "red": 0,
+                "orange": 0,
+                "yellow": 0,
+                "green": 0,
+                "blue": 0,
+                "white": 0,
+                "purple": 0
+            },
+            {
+                "red": 0,
+                "orange": 0,
+                "yellow": 0,
+                "green": 0,
+                "blue": 0,
+                "white": 0,
+                "purple": 0
+            },
+            {
+                "red": 0,
+                "orange": 0,
+                "yellow": 0,
+                "green": 0,
+                "blue": 0,
+                "white": 0,
+                "purple": 0
+            },
+            {
+                "red": 0,
+                "orange": 0,
+                "yellow": 0,
+                "green": 0,
+                "blue": 0,
+                "white": 0,
+                "purple": 0
+            }
+        ],
+    "slots" : [],
+    "elements" : [],
+    "crafting" : {
+        "craftable": False,
+        "previous": 0,
+        "branches": [],
+        "final" : False,
+        "cost" : 0,
+        "craftingMaterials": [],
+        "upgradeMaterials": []
+    },
+    "assets" : {
+        "icon": "",
+        "image": ""
+    }
+}
 
 details = soup.find_all(class_='col-sm-6')[0].text.strip().split('-')
 
@@ -20,13 +106,14 @@ affinity = statTableRow[4].text.split()[0].rstrip('%')
 weaponTreeTable = soup.find_all(class_='col-lg-6')[0]
 craftingTable = soup.find_all(class_='col-lg-6')[1].find(class_='table table-sm')
 craftingTableRow = craftingTable.find_all("tr")
+craftable = False
 for item in range(0,len(craftingTableRow)):
     craftingTableRow[item] = craftingTableRow[item].text.strip().split('\n')
     for line in range(0,len(craftingTableRow[item])):
         craftingTableRow[item][line] = craftingTableRow[item][line].strip()
-print(craftingTableRow)
-print('')
-
+        if craftingTableRow[item][line] == 'Forge Equipment':
+            craftable = True
+elderseal = str(statTableRow[7].find_all('strong')[0])[8:-9]
 
 obj["id"] = 0
 obj["type"] = details[0].strip()
@@ -37,7 +124,7 @@ obj["attack"] = {
     }
 obj["affinity"] = int(affinity)
 obj["defense"] = int(statTableRow[5].text.split()[0][1:-1])
-obj["elderseal"] = str(statTableRow[7].find_all('strong')[0])[8:-9]
+obj["elderseal"] = None if elderseal == '' else elderseal
 obj["attributes"] = {}
 obj["damageType"] = None
 obj["name"] = soup.find_all(class_='align-self-center')[0].text
@@ -101,11 +188,12 @@ obj["durability"] = [
 obj["slots"] = []
 obj["elements"] = []
 obj["crafting"] = {
-    "craftable": True,
+    "craftable": craftable,
     "previous": None,
     "branches": [
             2
         ],
+    "final" : 0,
     # "cost" : int(craftingTableRow[0].split('\n')[1].rstrip('z').replace(',','')),
     "craftingMaterials": [
         {
@@ -130,13 +218,22 @@ obj["assets"] = {
 
 
 elements = statTableRow[2].text.split()
+# print(elements)
 elements.pop()
 for x in range(0,len(elements),2):
-    obj['elements'].append({elements[x] : elements[x+1]})
+    
+    hidden = False
+    
+    if elements[x][0] == '(':
+        hidden = True
+        elements[x] = elements[x].replace('(','')
+        elements[x+1] = elements[x+1].replace(')','')
+    obj['elements'].append({
+        'type': elements[x],
+        'damage': int(elements[x+1]),
+        'hidden': hidden})
 
 for slot in slots:
     x = slot['src'][-5]
     obj['slots'].append({'rank' : int(x)})
 
-
-# print(obj['crafting'])
